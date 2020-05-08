8497b3df2d47f819cc00bf2cb431d5e1a1439c03
eroux <elie.roux@telecom-bretagne.eu>
2020-05-08 22:45:31 +0200

    advancing a bit

--- thumbnailgen.py
+++ thumbnailgen.py
@@ -5,16 +5,24 @@ import PIL.ImageOps
 import PIL.ImageCms
 import io
 import os
+import glob
 
 IIGITDIR="../xmltoldmigration/tbrc-ttl/iinstances/"
+BASE_MAX_DIM=370
+BASE_CROP_DIM=185
+MAX_RATIO=2
 
-def get_s3_folder_prefix(workRID, imageGroupID):
+BDR = Namespace("http://purl.bdrc.io/resource/")
+NSM = NamespaceManager(Graph())
+NSM.bind("bdr", BDR)
+
+def get_s3_folder_prefix(iiLocalName, igLocalName):
     """
     gives the s3 prefix (~folder) in which the volume will be present.
     inpire from https://github.com/buda-base/buda-iiif-presentation/blob/master/src/main/java/
     io/bdrc/iiif/presentation/ImageInfoListService.java#L73
     Example:
-       - workRID=W22084, imageGroupID=I0886
+       - iiLocalName=W22084, igLocalName=I0886
        - result = "Works/60/W22084/images/W22084-0886/
     where:
        - 60 is the first two characters of the md5 of the string W22084
@@ -22,26 +30,26 @@ def get_s3_folder_prefix(workRID, imageGroupID):
           * the image group ID without the initial "I" if the image group ID is in the form I\\d\\d\\d\\d
           * or else the full image group ID (incuding the "I")
     """
-    md5 = hashlib.md5(str.encode(workRID))
+    md5 = hashlib.md5(str.encode(iiLocalName))
     two = md5.hexdigest()[:2]
 
-    pre, rest = imageGroupID[0], imageGroupID[1:]
+    pre, rest = igLocalName[0], igLocalName[1:]
     if pre == 'I' and rest.isdigit() and len(rest) == 4:
         suffix = rest
     else:
-        suffix = imageGroupID
+        suffix = igLocalName
 
-    return 'Works/{two}/{RID}/images/{RID}-{suffix}/'.format(two=two, RID=workRID, suffix=suffix)
+    return 'Works/{two}/{RID}/images/{RID}-{suffix}/'.format(two=two, RID=iiLocalName, suffix=suffix)
 
 
 def print_all_errors():
     """ prints all the errors in the db in a human friendly way"""
     pass
 
-def gets3blob(bucket, s3imageKey):
+def gets3blob(bucket, s3Key):
     f = io.BytesIO()
     try:
-        bucket.download_fileobj(s3imageKey, f)
+        bucket.download_fileobj(s3Key, f)
         return f
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == '404':
@@ -49,33 +57,45 @@ def gets3blob(bucket, s3imageKey):
         else:
             raise
 
-def getIcc(image):
-    pass
+def getImageList(iiLocalName, igLocalName):
+    s3key = get_s3_folder_prefix(iiLocalName, igLocalName)+"dimensions.json"
+    blob = gets3blob(s3key)
+    if blob is None:
+        return None
+    return gzip.decompress(blob)
 
-def getImageList(igQname):
-    pass
-
-def findBestThumbnail(igQname, imageList):
-    pass
+def findBestThumbnailIdx(igQname, imageList, tbrcintroimages):
+    idx = tbrcintroimages+1
+    if igQname.startswith("bdr:W1FEMC"):
+        idx = 1
+    # if there's a very big image, use it as thumbnail
+    for i in range(min(len(imageList), 10)):
+        if imageList[i].size > 3000000:
+            idx = i
+            break
+    return idx
 
 def getImage(igQname, iiLocalName, imageFileName):
     pass
 
-def uploadThumbnail(blob, filename, iiLocalName, igQname):
-    pass
+def uploadThumbnail(blob, bucketname, filename, iiLocalName, igQname):
+    s3key = get_s3_folder_prefix(iiLocalName, igLocalName)+filename
+    s3 = boto3.resource('s3')
+    obj = s3.Object(bucketname, s3key)
+    obj.put(Body=blob)
 
 SRGB = PIL.ImageCms.createProfile('sRGB')
 
 def thumbnailizeAndWriteInfo(im, info):
-    if im.width > 2*im.height:
-        th = PIL.ImageOps.fit(im, (400,200), PIL.Image.LANCZOS, centering=(0,0))
-    elif im.height > 2*im.width:
-        th = PIL.ImageOps.fit(im, (200,400), PIL.Image.LANCZOS, centering=(0,0))
+    if im.width > MAX_RATIO*im.height:
+        th = PIL.ImageOps.fit(im, (BASE_MAX_DIM,BASE_CROP_DIM), PIL.Image.LANCZOS, centering=(0,0))
+    elif im.height > MAX_RATIO*im.width:
+        th = PIL.ImageOps.fit(im, (BASE_CROP_DIM,BASE_MAX_DIM), PIL.Image.LANCZOS, centering=(0,0))
     else:
-        factor = 400 / max(im.width, im.height)
+        factor = BASE_MAX_DIM / max(im.width, im.height)
         th = PIL.ImageOps.scale(im, factor, PIL.Image.LANCZOS)
     if th.mode != "1" and th.mode != "P":
-        th = PIL.ImageOps.autocontrast(th)
+        #th = PIL.ImageOps.autocontrast(th)
         icc_blob = im.info.get('icc_profile')
         if icc_blob is not None:
             f = io.BytesIO(icc_blob)
@@ -84,20 +104,25 @@ def thumbnailizeAndWriteInfo(im, info):
     return th
 
 def getThumbnailBlob(im):
-    with io.BytesIO() as output:
-        if im.mode == "1":
+    if im.mode == "1" or im.mode == "P":
+        with io.BytesIO() as output:
             im.save(output, format="png", optimize=True, pnginfo=None, exif=b"")
-        else:
-            im.save(output, format="jpeg", quality=100, optimize=True, progressive=True, icc_profile=None, exif=b"", subsampling=2, qtables="web_low")
-        return output.getvalue()
+            return output.getvalue()
+    else:
+        with io.BytesIO() as output:
+            #im.save(output, format="jpeg", quality=75, optimize=True, progressive=True, icc_profile=None, exif=b"", subsampling=2, qtables="web_low")
+            im.save(output, format="jpeg", quality=75, optimize=True, progressive=True, icc_profile=None, exif=b"", subsampling=2)
+            return output.getvalue()
 
 def listFiles(iiGitPath):
-    pass
+    return glob.glob(iiGitPath+'/**/W*.trig')
 
-def getThumbnailForManifest(manifestUrl):
+def getThumbnailForIIIFManifest(manifestUrl):
     pass
 
 def thumbnailForIiFile(iiFilePath, infodb, force=False):
+    g = Graph()
+    g.parse(iiFilePath, format="ttl")
     # read file
     # if status != released, pass
     # if not force and 
@@ -109,7 +134,7 @@ def thumbnailForIiFile(iiFilePath, infodb, force=False):
     # uploadthumbnail
     pass
 
-def test():
+def testThgen():
     for imgfilename in ["test/femc.jpeg", "test/modern.jpeg", "test/08860003.tif"]:
         im = PIL.Image.open(imgfilename)
         if im is None:
@@ -123,5 +148,8 @@ def test():
         with open(outfname,'wb') as out:
             out.write(thblob)
     print(data)
+
+def test():
+    print(listFiles("/home/eroux/BUDA/softs/xmltoldmigration/tbrc-ttl/iinstances"))
 
 test()
